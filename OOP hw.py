1fbe5546d3533c403129439a8c9174cb9727e550
Ryan Do <ryan_do1@baylor.edu>
2021-09-20 17:35:58 -0500

    chage 1

--- OOP hw.py
+++ OOP hw.py
@@ -1,1 +1,17 @@
-Ryan ffff
+class RetailItem:
+    def __init__(self,description,units,price):
+        self.__description = description
+        self.__units = units
+        self.__price = price
+    def set_description(self,description):
+        self._description = description
+    def set_units(self,units):
+        self.__units = units
+    def set_price(self,price):
+        self.__price = price
+    def get_description(self):
+        return self.__description
+    def get_units(self):
+        return self.__units
+    def get_price(self):
+        return self.__price
